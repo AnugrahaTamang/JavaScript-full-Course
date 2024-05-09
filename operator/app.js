e52452d02bd151313cfd09a16e9fa4a9e5bb617e
AnugrahaGomja <tamanganugraha24@gmail.com>
2024-05-09 23:48:20 +0545

    operator

---
+++ operator/app.js
@@ -0,0 +1,66 @@
+//Operator are symbols that are used to perform operation on operand.There are so many types of operator in javascript.
+//1. ASSIGNMENT OPERATOR(=,+=,\=,%=,-=,*=)
+// let a  = 8;
+// let b = 3;
+// console.log(a-=b); //5
+// console.log(a+=b); //11
+// console.log(a/=b);//2.66666
+// console.log(a%=b); // 3
+// console.log(a*=b); //24
+
+// 2. COMPARISON OPERATOR(==,===,!=,!==,>,<,>=,<=)
+// let a = 12;
+// let b =4;
+// console.log(a==b); //false 
+// console.log(a===b); //false 
+
+// //difference between (==) & (===)
+// let c = "12";
+// let d = 12;
+// console.log(c==d);//true
+// console.log(c===d); //false cause it's check strictly.
+
+// console.log(a>b);
+// console.timeLog(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a!=b); //true
+// console.log(a!==b); //true
+
+// 3. ARITHMETIC OPERATOR
+// let a = 2;
+// let b = 1;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a\b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(a);
+// console.log(a--);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(--a);
+// console.log(a);
+
+
+// 5. JAVASCRIPT BITWISE OPERATOR 
+// let a = 4;
+// let b = 2;
+// console.log(a&b); //0
+// console.log(a|b); //6
+// console.log(a>>b); //1
+// console.log(a<<b); //16
+// console.log(a^b); //6
+// console.log(~b); //-3
+// console.log(~a); //-5
+// console.log(a>>>b); //1
+
+// 6. LOGICAL OPERATOR  
+let a = 10;
+let b = 12;
+console.log(a && b);//12
+console.log(a || b);//10
+console.log(!(a==b)); //true
+
