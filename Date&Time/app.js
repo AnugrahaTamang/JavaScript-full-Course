40859b034a87f19785b80f3c21c1ef5d1e7f7d9d
AnugrahaGomja <tamanganugraha24@gmail.com>
2024-05-12 23:29:21 +0545

    add new

--- Date&Time/app.js
+++ Date&Time/app.js
@@ -1,31 +1,31 @@
-let date = new Date();
-console.log(date);
-console.log(date.toLocaleDateString());
-console.log(date.toDateString());
-console.log(date.toTimeString());
+// let date = new Date();
+// console.log(date);
+// console.log(date.toLocaleDateString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
 
-let mycreateDate = new Date(2024,02,13);
-console.log(mycreateDate);
-console.log(mycreateDate.toLocaleDateString());
-console.log(mycreateDate.toDateString());
+// let mycreateDate = new Date(2024,02,13);
+// console.log(mycreateDate);
+// console.log(mycreateDate.toLocaleDateString());
+// console.log(mycreateDate.toDateString());
 
-let myTimeStamp = new Date('2012-01-10');
-console.log(myTimeStamp);
-console.log(myTimeStamp.toDateString());
+// let myTimeStamp = new Date('2012-01-10');
+// console.log(myTimeStamp);
+// console.log(myTimeStamp.toDateString());
 
-let generateDate = new Date(2024,01,23,5,6);
-console.log(generateDate.toDateString());
+// let generateDate = new Date(2024,01,23,5,6);
+// console.log(generateDate.toDateString());
 
-let MyTimeStamp = Date.now();
-console.log(MyTimeStamp);
-console.log(Date.now());
-console.log(Date.now()/1000);
-console.log(Math.floor(Date.now()/1000));
+// let MyTimeStamp = Date.now();
+// console.log(MyTimeStamp);
+// console.log(Date.now());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date();
-console.log(newDate);
-console.log(newDate.getFullYear());
-console.log(newDate.getMonth());
-console.log(newDate.getTime().toLocaleString());
-console.log(newDate.getDay()+1);
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth());
+// console.log(newDate.getTime().toLocaleString());
+// console.log(newDate.getDay()+1);
 
